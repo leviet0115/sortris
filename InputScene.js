b720436afbb1db2a1f2118b9ac6548a83fecd391
leviet <leviet0115@gmail.com>
2024-01-23 21:52:52 +0200

    handle delete with spaceback

--- InputScene.js
+++ InputScene.js
@@ -4,10 +4,18 @@ class InputScene extends Phaser.Scene {
   }
 
   create() {
+    this.background = this.add
+      .rectangle(400, 300, 500, 400, 0x000000)
+      .setStrokeStyle(2, 0x00ff00, 1);
+
+    this.title = this.add
+      .text(400, 450, "Please enter admin pin code and press return.")
+      .setOrigin(0.5);
+
     this.inputTexts = [];
     for (let i = 0; i < 4; i++) {
       let inputText = this.add
-        .text(150 + i * 60, 200, "_", {
+        .text(300 + i * 60, 250, "_", {
           font: "48px Arial",
           fill: "#FFFFFF",
         })
@@ -21,6 +29,7 @@ class InputScene extends Phaser.Scene {
   }
 
   handleInput(event) {
+    //handle number input
     if (
       this.currentInputIndex < 4 &&
       event.keyCode >= 48 &&
@@ -31,19 +40,28 @@ class InputScene extends Phaser.Scene {
       this.currentInputIndex++;
     }
 
+    //handle delete number
+    if (event.keyCode === 8) {
+      this.inputTexts[this.currentInputIndex - 1].setText("_");
+      this.currentInputIndex -= 1;
+    }
+
+    //hanlde return pin code
     if (this.currentInputIndex === 4 && event.keyCode === 13) {
-      console.log("Submitted pin");
+      //console.log("Submitted pin");
       this.pinInput = this.inputTexts.map((text) => text.text).join("");
-      console.log(this.pinInput);
-      let isPinCorrect = this.pinInput === "0000";
+      //console.log(this.pinInput);
+      this.isPinCorrect = this.pinInput === "0000";
 
       this.inputTexts.forEach((text) =>
-        text.setFill(isPinCorrect ? "#0f0" : "red")
+        text.setFill(this.isPinCorrect ? "#0f0" : "red")
       );
-      if (isPinCorrect) {
-        this.scene.start("GameScene");
+      if (this.isPinCorrect) {
+        // setTimeout(() => this.scene.start("GameScene"), 500);
+        let userInput = prompt("Please enter your input:");
+        console.log("You entered:", userInput);
       } else {
-        this.cameras.main.shake(100, 0.01);
+        this.cameras.main.shake(100, 0.005);
         setTimeout(() => {
           this.inputTexts.forEach((text) => text.setText("_").setFill("#fff"));
           this.currentInputIndex = 0;
